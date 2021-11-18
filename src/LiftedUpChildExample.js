722eb99e2508728c1061b8c7ce7068ee36d52295
David <12038670+learnitmyway@users.noreply.github.com>
2021-11-18 07:18:36 +0100

    update lifted up child example

--- src/LiftedUpChildExample.js
+++ src/LiftedUpChildExample.js
@@ -9,11 +9,27 @@ const Component = ({ children }) => {
       <div className="forceRender">
         {renderCount}
         <button onClick={() => forceUpdate({})}>force render</button>
+        <span> Component</span>
       </div>
       {children}
     </div>
   );
 };
+
+const MemoComponent = React.memo(({ children }) => {
+  const [, forceUpdate] = React.useState();
+  const renderCount = useRenderCounter();
+  return (
+    <div className="frame">
+      <div className="forceRender">
+        {renderCount}
+        <button onClick={() => forceUpdate({})}>force render</button>
+        <span> MemoComponent</span>
+      </div>
+      {children}
+    </div>
+  );
+});
 
 const ChildComponent = () => {
   const renderCount = useRenderCounter();
@@ -25,6 +41,16 @@ const ChildComponent = () => {
   );
 };
 
+const MemoChildComponent = React.memo(() => {
+  const renderCount = useRenderCounter();
+  return (
+    <div className="frame">
+      {renderCount}
+      MemoChildComponent passed as prop
+    </div>
+  );
+});
+
 export default function LiftedUpChildExample() {
   const [, forceUpdate] = React.useState();
   const renderCount = useRenderCounter();
@@ -32,10 +58,18 @@ export default function LiftedUpChildExample() {
     <section>
       <h3>Example: Lifting a child above its parent</h3>
       <p>
-        Question: Does a child that is passed a prop re-render if its parent
+        Question 1: What happens if a child that is passed a prop to its parent
+        (ie lifted to the same level as its parent) re-render if its parent
         re-renders?
       </p>
-      <p>Answer: Only if its parent's parent re-renders</p>
+      <p>
+        Answer 1: The child re-rerenders when its grandparent re-renders unless
+        it is memoized.
+      </p>
+      <p>
+        Question 2: What if the parent is wrapped in <code>React.memo</code>?
+      </p>
+      <p>Answer 2: No effect</p>
       <div className="frame">
         <div className="forceRender">
           {renderCount}
@@ -43,7 +77,12 @@ export default function LiftedUpChildExample() {
         </div>
         <Component>
           <ChildComponent />
+          <MemoChildComponent />
         </Component>
+        <MemoComponent>
+          <ChildComponent />
+          <MemoChildComponent />
+        </MemoComponent>
       </div>
     </section>
   );
