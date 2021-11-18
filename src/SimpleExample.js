9f043705c999f69afed0706b01babbd19840844a
David <12038670+learnitmyway@users.noreply.github.com>
2021-11-18 16:28:02 +0100

    add props to simple example

--- src/SimpleExample.js
+++ src/SimpleExample.js
@@ -1,7 +1,8 @@
 import React from "react";
 import useRenderCounter from "./useRenderCounter";
 
-const Component = ({ name = "Component" }) => {
+const Component = ({ name = "Component", value }) => {
+  console.log(value);
   const renderCount = useRenderCounter();
   return (
     <div className="frame">
@@ -11,25 +12,30 @@ const Component = ({ name = "Component" }) => {
   );
 };
 
-const MemoComponent = React.memo(() => <Component name="MemoComponent" />);
+const MemoComponent = React.memo((props) => (
+  <Component {...props} name="MemoComponent" />
+));
 
 export default function Example1() {
   const [, forceUpdate] = React.useState();
+  const [value] = React.useState({ some: "value" });
   const renderCount = useRenderCounter();
   return (
     <section>
       <h3>Example: Parent child relationship</h3>
-      <p>Question: Does a child re-render if its parent re-renders?</p>
+      <p>Question 1: Does a child re-render if its parent re-renders?</p>
       <p>
-        Answer: Yes, unless its child is wrapped in <code>React.memo</code>.
+        Answer 1: Yes, unless its child is wrapped in <code>React.memo</code>.
       </p>
+      <p>Question 2: Is state memoized when passed as a prop?</p>
+      <p>Answer 2: Yes</p>
       <div className="frame">
         <div className="forceRender">
           {renderCount}
           <button onClick={() => forceUpdate({})}>force render</button>
         </div>
-        <Component />
-        <MemoComponent />
+        <Component value={value} />
+        <MemoComponent value={value} />
       </div>
     </section>
   );
